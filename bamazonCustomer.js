27ef2cac081c1f267636d4591478635f4f570d03
jimmy nagles <jimmynagles@gmail.com>
2020-03-04 18:42:38 -0800

    95% done got to fix connection querry

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,16 +19,84 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
 
-  console.log(chalk.red("connected as id "+connection.threadId));
+  console.log(chalk.red("connected as id " + connection.threadId));
   // run the start function after the connection is made to prompt the user
-  // start();
-  
+  showProducts();
+ 
 });
 
 
-  
- 
+function showProducts() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].stock_quantity + " | " + res[i].price);
+    }
+
+    getOrder();
+    
+    
+  });
+}
+
+function getOrder() {
+  inquirer
+    .prompt([
+
+      {
+        name: "id",
+        type: "input",
+        message: "what is the id of the produc youll like to buy? ",
+      },
+
+      {
+        name: "quantity",
+        type: "input",
+        message: "how many would you like to buy? ",
+      }
+
+    ])
+    .then(function (answer) {
+
+
+
+      console.log(answer.id)
+      console.log(answer.quantity)
+
+const id= answer.id;
+const quantity= answer.quantity;
+
+order(id,quantity);
+
+    });
+}
+
+function order(id, quantity){
+	connection.query('Select * FROM products WHERE id = ' + id, function(err,res){
+
+    if(err){console.log(err)};
+    
+
+		if(quantity <= res[0].stock_quantity){
+			var totalCost = res[0].price * quantity;
+			console.log("Good news your order is in stock!");
+			console.log("Your total cost for " + quantity + " " +res[0].product_name + " is " + totalCost + " Thank you!");
+
+      console.log(quantity)
+      console.log(id)
+      connection.query('UPDATE products SET stock_quantity = stock_quantity -' + quantity+ 'WHERE id' + id+' ');
+      console.log("UPDATE products SET stock_quantity = stock_quantity - 1 WHERE id = 1")
+
+		} else{
+			console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+    };
+    
+    showProducts();
+		
+	});
+};
 
